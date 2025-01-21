7949d65b1e251518ef2619de5a41cdb69a090bf1
Isha Savani <123747477+Ishasavani1402@users.noreply.github.com>
2025-01-21 12:45:05 +0530

    Update ChatServer.java

--- ChatServer.java
+++ ChatServer.java
@@ -1,44 +1,54 @@
 import java.io.*;
 import java.net.*;
-import java.util.Scanner;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+class Clienthandler implements Runnable{
+    private Socket client_socket;
+
+    public Clienthandler(Socket client_socket){
+        this.client_socket = client_socket;
+    }
+    @Override
+    public void run() {
+        try(BufferedReader br = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
+        PrintStream pr = new PrintStream(client_socket.getOutputStream(),true)){
+
+            String clientmsg;
+            while ((clientmsg=br.readLine())!=null) {
+                System.out.println("Client says :"+ clientmsg);
+                pr.println("Server received :"+clientmsg);
+
+                if(clientmsg.equalsIgnoreCase("exit")){
+                    System.out.println("Client Disconnect!!!");
+                    break;
+                }
+            }
+
+        }catch(IOException e){
+            System.out.println("Error Handling Client..."+e.getMessage());
+        }
+        
+    }
+}
 
 public class ChatServer {
     public static void main(String[] args) throws IOException {
         int port = 12345;
+        ExecutorService executor = Executors.newFixedThreadPool(10);// thread pool for multiple cloent size 10
         try (ServerSocket server_socket = new ServerSocket(port)) {
             System.out.println(" Server started. Listening for incoming connections...");
 
-            try (Socket client_socket = server_socket.accept();
-                    PrintWriter pr = new PrintWriter(client_socket.getOutputStream(), true); 
-                    BufferedReader br = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-                    Scanner sc = new Scanner(System.in)) {// Create a server socket on port 12345
-
-                    System.out.println("Client Connected..");
-
-                    Thread t = new Thread(() -> {
-                        try{
-                            String message ;
-                            while ((message = br.readLine())!=null) {
-                                System.out.println("client msg :" + message);   
-                            }
-                        }catch(IOException e){
-                            System.out.println(" Error in reading from client");
-                        }
-                    });
-
-                    t.start();
-
-                    //loop to get the message from the user(client)
-                while (true) {
-                    System.out.println("client says");
-                    String server_msg = sc.nextLine();
-                    pr.println(server_msg);
-                    if (server_msg.equalsIgnoreCase("exit")) {
-                        System.out.println("chat exit..");
-                        break;
-                    }
-                }
+            while (true) {
+                Socket client_socket = server_socket.accept();
+            
+                System.out.println("Client Connected.."+ client_socket.getInetAddress());
+                executor.execute(new Clienthandler(client_socket));
             }
         }
     }
 }
+
+
+
